29a89502e1b1bbf1fa0c5c1616682a65f7a07b66
Arkar <arkarchanmyaeko@gmail.com>
2025-05-13 15:58:00 +0700

    fix local storage bug

--- stores/useLanguagesStore.ts
+++ stores/useLanguagesStore.ts
@@ -1,8 +1,16 @@
 import { defineStore } from "pinia"
 
 export const useLanguagesStore = defineStore("languages", () => {
-  const languages = JSON.parse(localStorage.getItem("languages") || "[]")
-  const words = JSON.parse(localStorage.getItem("languageWords") || "[]")
-  return { languages, words }
+  const languages = ref<any>(JSON.parse(localStorage.getItem("languages") || "[]"))
+  const words = ref<any>(JSON.parse(localStorage.getItem("languageWords") || "[]"))
+  function setLanguages(languageValues: any) {
+    languages.value = languageValues
+    localStorage.setItem('languages', JSON.stringify(languageValues))
+  }
+  function setWords(wordValues: any) {
+    words.value = wordValues
+    localStorage.setItem('languageWords', JSON.stringify(wordValues))
+  }
+  return { languages, words, setLanguages, setWords }
 })
 
--- stores/usePeopleGroupsStore.ts
+++ stores/usePeopleGroupsStore.ts
@@ -1,11 +1,15 @@
 import { defineStore } from "pinia"
 
 export const usePeopleGroupStore = defineStore("peopleGroup", () => {
-  const peopleGroups = JSON.parse(localStorage.getItem("peopleGroups") || "{}")
+  const peopleGroups = ref<any>(JSON.parse(localStorage.getItem("peopleGroups") || "{}"))
+  function setPeopleGroups(peopleGroupValues: any) {
+    peopleGroups.value = peopleGroupValues
+    localStorage.setItem('peopleGroups', JSON.stringify(peopleGroupValues))
+  }
   const options = computed(() => {
-    return peopleGroups.map((pg: any) => ({ label: pg.name, value: pg.id }))
+    return peopleGroups.value.map((pg: any) => ({ label: pg.name, value: pg.id }))
   })
 
-  return { peopleGroups, options }
+  return { peopleGroups, options, setPeopleGroups }
 })
 
